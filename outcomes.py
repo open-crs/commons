3d3ed78a66b1f377f116bf5b1240a9e6aa32cf1c
George-Andrei Iosif <georgeiosif@live.com>
2023-05-24 22:20:10 +0300

    Adds possible outcomes of exploits

---
+++ outcomes.py
@@ -0,0 +1,10 @@
+from enum import Enum, auto
+
+
+class Outcome(Enum):
+    CODE_EXECUTION = 0
+    CALL_TO_WIN_FUNCTION = CODE_EXECUTION + 1
+    INTERACTIVE_SHELL = CODE_EXECUTION + 2
+
+    INFORMATION_LEAK = 10
+    DENIAL_OF_SERVICE = 20
